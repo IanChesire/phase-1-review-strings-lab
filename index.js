0e98e3a8683599ab21c4f7999fe2d03cbf97e0e0
Ian <iankosgey535@gmail.com>
2023-12-11 17:25:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Tumoo Yan';
+
+const welcomeMessage = "Welcome to Flatbook, " + 'Tumoo Yan!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + 'TUMOO YAN!';
+const shortGreeting = 'Welcome, T !';
